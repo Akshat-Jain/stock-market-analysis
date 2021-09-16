d1c3887c8cfd3545009576869eb2fe80e0d95156
Tanay Maheshwari <tanaymodani18@gmail.com>
2021-09-16 18:45:54 +0530

    corrections

--- send_mail.py
+++ send_mail.py
@@ -1,6 +1,8 @@
 import base64
 import os
 from dotenv import load_dotenv
+from datetime import date
+today = date.today()
 load_dotenv()
 from sendgrid.helpers.mail import (
     Mail, Attachment, FileContent, FileName,
@@ -14,16 +16,16 @@ to_emails = [
 
 def send_mail(fromMail, toMail,msg):
     message = Mail(
-        from_email=('tanaymodani18@gmail.com', 'Tanay'),
-        to_emails=to_emails,
-        subject='Stocks List',
+        from_email=('tanaymodani18@gmail.com', 'Stock Market Analysis'),
+        to_emails='tanaymodani18@gmail.com',
+        subject='[Stocks Analysis] Strategy 1:  '+today.strftime("%B %d, %Y"),
         html_content=msg)
     try:
         sendgrid_client = SendGridAPIClient(os.getenv('SENDGRID_API_KEY'))
         response = sendgrid_client.send(message)
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
+        # print(response.status_code)
+        # print(response.body)
+        # print(response.headers)
     except Exception as e:
         print(e)
 
